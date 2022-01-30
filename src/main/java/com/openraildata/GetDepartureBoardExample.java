6b7332a296a9dda2f835833544bb30be9382bfd5
Richard Stokes <rjstokes.lp@gmail.com>
2022-01-30 17:09:42 +0000

    Adding initial station code

---
+++ src/main/java/com/openraildata/GetDepartureBoardExample.java
@@ -0,0 +1,101 @@
+package com.openraildata;
+
+import com.thalesgroup.rtti._2013_11_28.token.types.AccessToken;
+import com.thalesgroup.rtti._2017_10_01.ldb.*;
+import com.thalesgroup.rtti._2017_10_01.ldb.types.ArrayOfArrayOfCallingPoints;
+import com.thalesgroup.rtti._2017_10_01.ldb.types.ArrayOfCallingPoints;
+import com.thalesgroup.rtti._2017_10_01.ldb.types.CallingPoint;
+import com.thalesgroup.rtti._2017_10_01.ldb.types.ServiceItem;
+import org.apache.cxf.ext.logging.LoggingInInterceptor;
+import org.apache.cxf.ext.logging.LoggingOutInterceptor;
+import org.apache.cxf.frontend.ClientProxy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.naming.ConfigurationException;
+import java.util.List;
+
+/**
+ * Open Live Departure Boards Web Service (OpenLDBWS) API Demonstrator
+ * Copyright (C)2018-2021 OpenTrainTimes Ltd.
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+public class GetDepartureBoardExample {
+
+    private static final Logger logger = LoggerFactory.getLogger(GetDepartureBoardExample.class);
+
+    private static final String LDB_TOKEN = "22f9c534-2a6f-44cf-8612-5cf7eb958e6c";
+    private static final boolean DEBUG = false;
+
+    public static void main(String[] args) throws ConfigurationException {
+
+        if (LDB_TOKEN.isEmpty()) {
+            throw new ConfigurationException("Please configure your OpenLDBWS token in GetDepartureBoardExample!");
+        }
+
+        var accessToken = new AccessToken();
+        accessToken.setTokenValue(LDB_TOKEN);
+
+        var soap = new Ldb();
+        var soapService = soap.getLDBServiceSoap12();
+
+        /*
+         * To examine the request and responses sent to the service, set DEBUG to true above
+         */
+        if (DEBUG) {
+            var client = ClientProxy.getClient(soapService);
+            client.getInInterceptors().add(new LoggingInInterceptor());
+            client.getOutInterceptors().add(new LoggingOutInterceptor());
+        }
+
+        var params = new GetBoardRequestParams();
+        params.setCrs("HYS");
+
+        StationBoardResponseType departureBoard = soapService.getDepartureBoard(params, accessToken);
+
+        logger.info("Trains at {}", departureBoard.getGetStationBoardResult().getLocationName());
+        logger.info("===============================================================================");
+
+        List<ServiceItem> service = departureBoard.getGetStationBoardResult().getTrainServices().getService();
+
+        for (ServiceItem si : service) {
+
+            logger.info("{} to {} - {}", si.getStd(), si.getDestination().getLocation().get(0).getLocationName(), si.getEtd());
+
+        }
+
+        GetServiceDetailsRequestParams serviceParams = new GetServiceDetailsRequestParams();
+        serviceParams.setServiceID(service.get(0).getServiceID());
+        ServiceDetailsResponseType serviceItem = soapService.getServiceDetails(serviceParams, accessToken);
+
+
+        ArrayOfArrayOfCallingPoints callingPoints = serviceItem.getGetServiceDetailsResult().getSubsequentCallingPoints();
+
+        List<ArrayOfCallingPoints> points = callingPoints.getCallingPointList();
+
+        logger.info("Sizwe: "+points.size());
+        for(ArrayOfCallingPoints point:points){
+            List<CallingPoint> call = point.getCallingPoint();
+            for(CallingPoint finalPoint:call){
+                logger.info(finalPoint.getLocationName()+" "+finalPoint.getEt());
+            }
+
+        }
+
+
+
+    }
+
+}
